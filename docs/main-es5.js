516b80d6c09f2ff8e7e2d8fd07157a6bac24dca1
hoangson23 <hoangson.hust@gmail.com>
2021-05-30 10:23:21 +0700

    A

--- docs/main-es5.js
+++ docs/main-es5.js
@@ -829,7 +829,7 @@
         i18nPrefix: '',
         //apiUrl: 'https://localhost:5005',
         //apiUrl: 'https://localhost:44360',
-        apiUrl: 'https://18.222.113.192',
+        apiUrl: 'https://3.133.133.81',
         //apiUrl: 'https://bookyapideploy.azurewebsites.net',
         //////////////////////////////////////////////////////////////////
         //clientUrlForgotPassword: 'http://localhost:4200/password/reset-password',
